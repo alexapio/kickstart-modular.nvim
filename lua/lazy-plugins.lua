b5fb4528ec695e8840a03dc487d3cf4c290c3055
Alejandro Celery <alejandro.celery@fppnt.com>
2025-07-21 16:10:58 +0100

    Enable vim-tmux-navigator
    See matching tmux.conf config

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -33,6 +33,7 @@ require('lazy').setup({
   require 'kickstart.plugins.todo-comments',
   require 'kickstart.plugins.mini',
   require 'kickstart.plugins.treesitter',
+  require 'kickstart.plugins.vim-tmux-navigator',
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
