c3a6f9828f5723b38f98ee44a897e18f8d5666c8
Alejandro Celery <alejandro.celery@fppnt.com>
2026-02-09 09:24:03 +0000

    Personalization

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -31,6 +31,10 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Make vertical scrolling faster
+vim.keymap.set('n', '<C-e>', '4<C-e>', { desc = 'Scroll faster' })
+vim.keymap.set('n', '<C-y>', '4<C-y>', { desc = 'Scroll faster' })
+
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
 -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
